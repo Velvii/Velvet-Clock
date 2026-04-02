6c63aca31124a4d1ccc5ce0ba17d6465f811cc47
Eli Moretti <144716146+EliMorettiCS@users.noreply.github.com>
2026-04-02 08:34:24 -0700

    Initial Commit (lol)

---
+++ main.js
@@ -0,0 +1,77 @@
+const delay = ms => new Promise(res => setTimeout(res, ms*1000));
+console.clear()
+
+let mode
+let military = false
+let secondsDisplay = true;
+let clockAnimation = true;
+console.log()
+
+mode = "clock"
+let hourEl = document.getElementById("hour")
+let colon1El = document.getElementById("colon1")
+let minuteEl = document.getElementById("minute")
+let colon2El = document.getElementById("colon2")
+let secondEl = document.getElementById("second")
+let meridiemEl = document.getElementById("meridiem")
+
+let clockButtonEl = document.getElementById("clockButton")
+let timerButtonEl = document.getElementById("timerButton")
+
+
+clockButtonEl.onclick = function(){
+
+}
+
+
+function updateClock(){
+    if (mode == "clock"){
+    const currentDate = new Date()
+    if (military){
+        hourEl.textContent = currentDate.getHours()
+    }
+    else{
+        if (currentDate.getHours() > 12){
+            hourEl.textContent = currentDate.getHours()-12
+            meridiemEl.textContent = "PM"
+        }
+        else{
+            meridiemEl.textContent = "AM"
+        }
+    }
+    if (currentDate.getMinutes() <= 10){
+        minuteEl.textContent = "0"+currentDate.getMinutes().toString()
+        
+    }
+    else{
+        minuteEl.textContent = currentDate.getMinutes()
+    }
+    if (secondsDisplay){
+        if (currentDate.getSeconds() <= 10){
+            if (clockAnimation){
+                secondEl.style.animation = 'none';
+                secondEl.offsetHeight;
+                secondEl.style.animation = "textPop .1s"
+            }
+            secondEl.textContent = "0"+currentDate.getSeconds().toString()
+        }
+        else{
+            if (clockAnimation){
+                secondEl.style.animation = 'none';
+                secondEl.offsetHeight;
+                secondEl.style.animation = "textPop .1s"
+            }
+            secondEl.textContent = currentDate.getSeconds()
+        }
+    }
+    else{
+        secondEl.remove()
+        colon2El.remove()
+    }
+    }
+    if (military){
+        meridiemEl.remove()
+    }
+}
+updateClock()
+setInterval(updateClock,1000)
